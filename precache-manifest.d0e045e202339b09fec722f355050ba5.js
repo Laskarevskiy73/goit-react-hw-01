5765df24620049753c210b6f29daafb39852d810
Laskarevskiy_Andrey <laskarevskiy73@gmail.com>
2019-04-27 20:47:08 +0300

    Updates

---
+++ precache-manifest.d0e045e202339b09fec722f355050ba5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ab567a83dfa4a49f74a4",
+    "url": "/goit-react-hw-01/static/css/main.cbb74476.chunk.css"
+  },
+  {
+    "revision": "ab567a83dfa4a49f74a4",
+    "url": "/goit-react-hw-01/static/js/main.13421895.chunk.js"
+  },
+  {
+    "revision": "f51632aec18d5636ad74",
+    "url": "/goit-react-hw-01/static/js/runtime~main.bdf556df.js"
+  },
+  {
+    "revision": "294a3c23aa9869ae810b",
+    "url": "/goit-react-hw-01/static/js/2.7272bfd8.chunk.js"
+  },
+  {
+    "revision": "72d4c9effd7070442cf94c68fc115d33",
+    "url": "/goit-react-hw-01/index.html"
+  }
+];
